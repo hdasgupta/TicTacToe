1f77d691af32bae644a812b010b1353aa42734c5
Himaghna Dasgupta <himaghna.dasgupta@arthmate.com>
2022-04-13 10:26:25 +0530

    Initial

--- src/main/kotlin/com/tictactoe/Services.kt
+++ src/main/kotlin/com/tictactoe/Services.kt
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component
 @Component
 class Services:CommandLineRunner {
     var running: Boolean = true
-        get() = running
 
     override fun run(vararg args: String?) {
         Thread {
